32eb7f9fcd1f12476d359072226d4fb453eec477
shulyar <andy.shular@gmail.com>
2022-10-11 22:33:51 +0300

    edit 1 task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,46 +1,72 @@
 import { galleryItems } from './gallery-items.js';
+import * as basicLightbox from '/node_modules/basiclightbox/src/scripts/main.js'
 
 
 
 // Change code below this line
 
-
 let galleryEl = document.querySelector(".gallery");
 galleryEl.classList.add("gallery");
 
 
+function initGallery() {
 
+    galleryItems.forEach(element => {
 
+        let galleryItemEl = document.createElement("div");
+        galleryItemEl.classList.add("gallery__item");
+        galleryEl.append(galleryItemEl);
 
-galleryItems.forEach(element => {
+        let galleryLinkEl = document.createElement("a");
+       // galleryLinkEl.innerHTML = element.original;
+        galleryLinkEl.classList.add("gallery__link");
+        galleryItemEl.append(galleryLinkEl);
 
-    let galleryItemEl = document.createElement("div");
-    galleryItemEl.classList.add("gallery__item");
-    galleryEl.append(galleryItemEl);
+        let imgEl = document.createElement("img");
+        imgEl.classList.add("gallery__image");
+        imgEl.setAttribute("src", element.preview);
+        imgEl.setAttribute("alt", element.description);
+        imgEl.setAttribute("data-source", element.original);
+        galleryLinkEl.append(imgEl);
 
-    let galleryLinkEl = document.createElement("a");
-    galleryLinkEl.innerHTML = element.original;
-    galleryLinkEl.classList.add("gallery__link");
-    galleryItemEl.append(galleryLinkEl);
+    
 
-    let imgEl = document.createElement("img");
-    imgEl.classList.add("gallery__image");
-    imgEl.setAttribute("src", element.preview);
-    imgEl.setAttribute("alt", element.description);
-    imgEl.setAttribute("data-source", element.original);
-    galleryLinkEl.append(imgEl);
-
-
-
-});
+    });
+};
+ 
+initGallery();
 
 galleryEl.addEventListener("click", (event) => {
     if (event.target.nodeName === "IMG") {
-        console.log("ha ha ha");
-        galleryEl.style.backgroundImage = event.target.currentSrc;
+      
+      
+        let instance = basicLightbox.create(`
+            <div class="modal">
+            <img src="${event.target.currentSrc}" width="1200" height="800">
+            </div>
+        `);
+        
+        galleryEl.style.display = 'none';
+        
+        instance.show()
+        
 
     } else {
         console.log("border!!!!");
+    }
+
+});
+
+document.body.addEventListener("keydown", (event) => {
+    if (event.key === "Escape") {
+       // console.log("ha ha ha");
+        
+        galleryEl.style.display = 'grid';
+        let moduleEl = document.querySelector(".basicLightbox");
+        moduleEl.remove();
+        
+    } else {
+        console.log("press escape to exit in gallery!!!!");
     }
 
 });
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,13 +1,11 @@
-import * as basicLightbox from '../node_modules/basiclightbox'
+import * as basicLightbox from '/node_modules/basiclightbox/src/scripts/main.js'
 
 const instance = basicLightbox.create(`
     <div class="modal">
-        <p>
-            Your first lightbox with just a few lines of code.
-            Yes, it's really that simple.
-        </p>
+      <img src="assets/images/image.png" width="800" height="600">
+
     </div>
-`)
+`);
 
 instance.show()
 console.log(galleryItems);
