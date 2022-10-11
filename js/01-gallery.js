09e6fb6c8402a27c4b27c11ebcc5a37d795a5d59
shulyar <andy.shular@gmail.com>
2022-10-11 07:36:08 +0300

    edit 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,10 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+galleryEl = document.querySelector(".gallery");
+galleryItemEl = document.createElement("div");
+galleryItemEl.classList.append("gallery__item");
+galleryEl.append(galleryItemEl);
+galleryLinkEl = document.createElement("a");
+
 
 console.log(galleryItems);
