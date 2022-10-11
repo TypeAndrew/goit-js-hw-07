21f4e620e9b96b94d309680285613cb9e07d2f76
TypeAndrew <andy.shular@gmail.com>
2022-10-11 15:57:39 +0300

    edit to gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,10 +1,50 @@
 import { galleryItems } from './gallery-items.js';
+
+
+
 // Change code below this line
-galleryEl = document.querySelector(".gallery");
-galleryItemEl = document.createElement("div");
-galleryItemEl.classList.append("gallery__item");
-galleryEl.append(galleryItemEl);
-galleryLinkEl = document.createElement("a");
+
+
+let galleryEl = document.querySelector(".gallery");
+galleryEl.classList.add("gallery");
+
+
+
+
+
+galleryItems.forEach(element => {
+
+    let galleryItemEl = document.createElement("div");
+    galleryItemEl.classList.add("gallery__item");
+    galleryEl.append(galleryItemEl);
+
+    let galleryLinkEl = document.createElement("a");
+    galleryLinkEl.innerHTML = element.original;
+    galleryLinkEl.classList.add("gallery__link");
+    galleryItemEl.append(galleryLinkEl);
+
+    let imgEl = document.createElement("img");
+    imgEl.classList.add("gallery__image");
+    imgEl.setAttribute("src", element.preview);
+    imgEl.setAttribute("alt", element.description);
+    imgEl.setAttribute("data-source", element.original);
+    galleryLinkEl.append(imgEl);
+
+
+
+});
+
+galleryEl.addEventListener("click", (event) => {
+    if (event.target.nodeName === "IMG") {
+        console.log("ha ha ha");
+        galleryEl.style.backgroundImage = event.target.currentSrc;
+
+    } else {
+        console.log("border!!!!");
+    }
+
+});
+
 
 
 console.log(galleryItems);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,13 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import * as basicLightbox from '../node_modules/basiclightbox'
 
+const instance = basicLightbox.create(`
+    <div class="modal">
+        <p>
+            Your first lightbox with just a few lines of code.
+            Yes, it's really that simple.
+        </p>
+    </div>
+`)
+
+instance.show()
 console.log(galleryItems);
